d1cc30d2b994d143cb9683d53e41cde8b3239e01
Rob Smith <r.smith251093@gmail.com>
2022-09-13 21:55:41 +0100

    Add etch a sketch grid and hover functionality

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-function addDiv(gridNum) {
-    const divContainer = document.getElementById("grid-container")
+function divGrid(rows, columns) {
+    const gridContainer = document.getElementById("grid-container")
 
-    for (let i = 0; i < gridNum; i++) {
-        const newDiv = document.createElement("div");
-        divContainer.appendChild(newDiv);
-    }
+    for (let i = 0; i <= columns; i++) {
+        const column = document.createElement("div");
+        column.className = "column";
+        for (let j = 0; j <= rows; j++) {
+            const row = document.createElement("div");
+            row.className = "row";
+            column.appendChild(row);
+        }
+        gridContainer.appendChild(column);
+    }       
 }
 
-addDiv(16);
+divGrid(16, 16);
+
+const gridDiv = document.getElementsByClassName("row");
+
+for (let i = 0; i < gridDiv.length; i++) {
+    gridDiv[i].addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+    })
+}
+
+
