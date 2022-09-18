59568489a30a609b3ed02e1e066b0fda51bcf0c8
Rob Smith <r.smith251093@gmail.com>
2022-09-18 19:12:53 +0100

    Rewrite div grid func and add user prompt for size

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-function divGrid(rows, columns) {
-    const gridContainer = document.getElementById("grid-container")
+const userBtn = document.getElementById("gridSizeBtn").onclick = userPrompt;
+const gridContainer = document.getElementById("grid-container");
+const currentGridSize = document.getElementById("current-size");
 
-    for (let i = 0; i <= columns; i++) {
-        const column = document.createElement("div");
-        column.className = "column";
-        for (let j = 0; j <= rows; j++) {
-            const row = document.createElement("div");
-            row.className = "row";
-            column.appendChild(row);
-        }
-        gridContainer.appendChild(column);
-    }       
+let gridSize = 16;
+
+function currentSize(gridSize) {
+    currentGridSize.innerText = `Current grid size is: ${gridSize} * ${gridSize}`;
 }
 
-divGrid(16, 16);
+function createGrid(gridSize) {
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;     
+    
+    for (let i= 0; i < gridSize * gridSize; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.id = "grid-element";
 
-const gridDiv = document.getElementsByClassName("row");
-
-for (let i = 0; i < gridDiv.length; i++) {
-    gridDiv[i].addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "black";
-    })
+        gridDiv.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black";
+        })
+        gridContainer.appendChild(gridDiv);
+    }
 }
+
+function deleteGrid() {    
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function userPrompt() {
+    gridSize = prompt("Please enter a desired grid size less than 100.");
+
+    if (gridSize > 100) {
+        alert("Grid size greater than 100, try again.");
+        return;
+    }
+
+    deleteGrid();
+    createGrid(gridSize);
+    currentSize(gridSize);
+}
+
+createGrid(gridSize);
+currentSize(gridSize);
 
 
